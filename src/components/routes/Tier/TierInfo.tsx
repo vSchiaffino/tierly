2683a78e14a963a4292b9c051099f1fd44728339
Valentin Schiaffino <valenschg@gmail.com>
2023-02-03 22:54:21 -0300

    responsive config

--- src/components/routes/Tier/TierInfo.tsx
+++ src/components/routes/Tier/TierInfo.tsx
@@ -9,15 +9,15 @@ interface Props {
 export const TierInfo: React.FC<Props> = ({ tier }) => {
   const { title, description, imageUrl } = tier
   return (
-    <main className="flex flex-row my-10 bg-slate-100 rounded-md p-10 shadow-sm">
-      <div className="flex-col flex-grow flex gap-5">
+    <main className="grid gap-10 lg:grid-cols-[1fr_auto] grid-cols-1 my-10 bg-slate-100 rounded-md p-10 shadow-sm">
+      <div className="flex flex-col gap-5">
         <Heading text={title} />
         <p className="text-lg text-slate-800">{description}</p>
       </div>
       <img
         src={imageUrl}
         alt={`Tier of ${title}`}
-        className="object-cover aspect-square flex-col"
+        className="mx-auto object-cover aspect-square flex-col"
       />
     </main>
   )
